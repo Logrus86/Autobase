ad802cab4b2ca33e0564103c4e36011dc68d0aea
Logrus86 <logr86@gmail.com>
2014-08-27 21:44:02 +0600

    dateParser

--- src/main/java/com/epam/bp/autobase/util/DateParser.java
+++ src/main/java/com/epam/bp/autobase/util/DateParser.java
@@ -4,7 +4,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Parser {
+public class DateParser {
+
     public static Date StringToDate(String stringDate) {
         Date result = null;
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
@@ -12,8 +13,12 @@ public class Parser {
             result = sdf.parse(stringDate);
         } catch (ParseException e) {
             e.printStackTrace();
-            //TODO add own exception and logging (in it)
         }
         return result;
+    }
+
+    public static String DateToString(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); //SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
+        return sdf.format(date);
     }
 }
