843f8287e008cf3c752d1df944666e076e5c4e75
Logrus86 <logr86@gmail.com>
2014-08-27 23:37:32 +0600

    +filters

---
+++ src/main/java/com/epam/bp/autobase/filter/cacheControlFilter.java
@@ -0,0 +1,4 @@
+package com.epam.bp.autobase.filter;
+
+public class cacheControlFilter {
+}
--- src/main/java/com/epam/bp/autobase/filter/charsetFilter.java
+++ src/main/java/com/epam/bp/autobase/filter/charsetFilter.java
@@ -1,13 +1,11 @@
 package com.epam.bp.autobase.filter;
 
-import com.epam.bp.autobase.util.Logger;
-
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class urlFilter extends Logger implements Filter{
+public class charsetFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
@@ -16,20 +14,12 @@ public class urlFilter extends Logger implements Filter{
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         doFilter((HttpServletRequest) request, (HttpServletResponse) response, chain);
     }
-
     private void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        String pathInfo = req.getRequestURI().substring(req.getContextPath().length());
-
-        if (pathInfo.startsWith("/static/")||pathInfo.startsWith("/webjars/")) {
-    //        LOGGER.info("url starts with '/static/' or '/webjars/', filtered off.");
-            chain.doFilter(req, resp);
-            return;
-        }
-        req.getRequestDispatcher(pathInfo).forward(req, resp);
+        req.setCharacterEncoding("UTF-8");
+        chain.doFilter(req, resp);
     }
 
     @Override
     public void destroy() {
-
     }
 }
