370c2a57a2e02a1f02ddf06975850061d405a5db
Logrus86 <logr86@gmail.com>
2014-09-03 17:44:07 +0600

    logging session attributes for experiment

--- src/main/java/com/epam/bp/autobase/listener/SessionListener.java
+++ src/main/java/com/epam/bp/autobase/listener/SessionListener.java
@@ -1,9 +1,13 @@
 package com.epam.bp.autobase.listener;
 
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+import java.util.Enumeration;
 
 public class SessionListener implements HttpSessionListener {
+    public final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(super.getClass());
     @Override
     public void sessionCreated(HttpSessionEvent se) {
 
@@ -11,7 +15,8 @@ public class SessionListener implements HttpSessionListener {
 
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
-     //   se.getSession().getAttributeNames();
 
+        for (Enumeration<String> attrs = se.getSession().getAttributeNames(); attrs.hasMoreElements(); )
+            LOGGER.info(attrs.nextElement());
     }
 }
