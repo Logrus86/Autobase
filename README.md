b184be294acec77b4918ae2a88b3f20c2da39109
Logrus86 <logr86@gmail.com>
2014-09-22 00:52:43 +0600

    forward dispatcher for charset filter, small changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Autobase.
 ========================
+//TODO possibility to change site language. save it to user? or cookie? or db?
+На бумагу:
+общее описание, список всех юзкейзов
--- src/main/java/com/epam/bp/autobase/Runner.java
+++ src/main/java/com/epam/bp/autobase/Runner.java
@@ -1,16 +1,10 @@
 package com.epam.bp.autobase;
 
-import com.epam.bp.autobase.entity.Autobase;
-import com.epam.bp.autobase.entity.AutobaseFactory;
-
 import java.sql.SQLException;
 
 public class Runner { //TODO delete this class later
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException, InterruptedException {
-        Autobase ab = AutobaseFactory.createRandomAutobase(10);
-        System.out.println(ab);
-
 
     }
 }
