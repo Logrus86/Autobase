a63c877361b37b534ccf2d8da9623932133eb07e
Logrus86 <logr86@gmail.com>
2014-09-23 00:45:18 +0600

    dao type stored at properties, DaoFactory uses reflection

--- src/main/java/com/epam/bp/autobase/dao/DaoFactory.java
+++ src/main/java/com/epam/bp/autobase/dao/DaoFactory.java
@@ -1,20 +1,23 @@
 package com.epam.bp.autobase.dao;
 
-import com.epam.bp.autobase.dao.H2.H2DaoFactory;
-
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public abstract class DaoFactory<Context> {
-    private static final ResourceBundle RB = ResourceBundle.getBundle("db");
-    private static final String DAOTYPE = RB.getString("dao.type");
-
+    private static final String FACTORY_CLASS = ResourceBundle.getBundle("db").getString("dao.factoryClassName");
     public static DaoFactory instance;
+
     public abstract DaoManager getDaoManager();
-    public abstract Context getContext() throws DaoException, ClassNotFoundException, InterruptedException;
-    public abstract void releaseContext() throws SQLException;
-    public static DaoFactory getInstance() throws DaoException, InterruptedException, ClassNotFoundException {
-        if (DAOTYPE.equals("h2")) instance = H2DaoFactory.getInstance();
+
+    public abstract Context getContext() throws DaoException;
+
+    public abstract void releaseContext() throws DaoException;
+
+    public static DaoFactory getInstance() throws DaoException {
+        try {
+            instance = (DaoFactory) Class.forName(FACTORY_CLASS).getMethod("getInstance").invoke(null);
+        } catch (Exception e) {
+            throw new DaoException("Error while get DaoFactory instance", e);
+        }
         return instance;
     }
 }
