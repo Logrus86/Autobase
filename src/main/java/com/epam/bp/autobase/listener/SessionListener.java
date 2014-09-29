27b980db0f2e21141cad76d5de4e75cf9fd95bd3
Logrus86 <logr86@gmail.com>
2014-09-30 02:50:44 +0600

    +SessionListener sets all data lista from Autobase to session attributes

--- src/main/java/com/epam/bp/autobase/listener/SessionListener.java
+++ src/main/java/com/epam/bp/autobase/listener/SessionListener.java
@@ -1,12 +1,41 @@
 package com.epam.bp.autobase.listener;
 
+import com.epam.bp.autobase.entity.Autobase;
+import com.epam.bp.autobase.entity.User;
+import com.epam.bp.autobase.entity.Vehicle;
+
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+import java.util.Locale;
 
 public class SessionListener implements HttpSessionListener {
+    private static final String FUEL_TYPES = "fuelTypes";
+    private static final String ROLES = "roles";
+    private static final String LIST_USERS = "userList";
+    private static final String LIST_CARS = "carList";
+    private static final String LIST_BUSES = "busList";
+    private static final String LIST_TRUCKS = "truckList";
+    private static final String LIST_COLORS = "colorList";
+    private static final String LIST_MODELS = "modelList";
+    private static final String LIST_MANUFS = "manufacturerList";
+    private static final String LOCALE = "locale";
+    private static final Locale DEFAULT_LOCALE = Locale.getDefault();
+
     @Override
     public void sessionCreated(HttpSessionEvent se) {
-
+        HttpSession session = se.getSession();
+        session.setAttribute(FUEL_TYPES, Vehicle.Fuel.values());
+        session.setAttribute(ROLES, User.Role.values());
+        Autobase autobase = Autobase.getInstance();
+        session.setAttribute(LIST_USERS, autobase.getUserList());
+        session.setAttribute(LIST_CARS, autobase.getCarList());
+        session.setAttribute(LIST_BUSES, autobase.getBusList());
+        session.setAttribute(LIST_TRUCKS, autobase.getTruckList());
+        session.setAttribute(LIST_COLORS, autobase.getColorList());
+        session.setAttribute(LIST_MODELS, autobase.getModelList());
+        session.setAttribute(LIST_MANUFS, autobase.getManufacturerList());
+        session.setAttribute(LOCALE, DEFAULT_LOCALE);
     }
 
     @Override
