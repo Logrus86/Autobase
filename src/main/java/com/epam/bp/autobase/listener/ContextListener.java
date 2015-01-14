8d14c4f5ff7729419d6ddd8f16aaa36332cb42e2
Logrus86 <logr86@gmail.com>
2015-01-14 09:11:38 +0600

    context attributes are being set in context if dao type H2 only

--- src/main/java/com/epam/bp/autobase/listener/ContextListener.java
+++ src/main/java/com/epam/bp/autobase/listener/ContextListener.java
@@ -1,28 +1,30 @@
 package com.epam.bp.autobase.listener;
 
-import com.epam.bp.autobase.entity.Order;
-import com.epam.bp.autobase.entity.User;
-import com.epam.bp.autobase.entity.Vehicle;
 import com.epam.bp.autobase.pool.ConnectionPool;
 import com.epam.bp.autobase.util.AttributeSetter;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.util.ResourceBundle;
 
 public class ContextListener implements ServletContextListener {
 
     private static final String COLOR = "Color";
     private static final String MODEL = "Model";
     private static final String MANUFACTURER = "Manufacturer";
+    private static final ResourceBundle RB = ResourceBundle.getBundle("dao");
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
 
-        ServletContext context = sce.getServletContext();
-        AttributeSetter.setEntityToContext(COLOR, context);
-        AttributeSetter.setEntityToContext(MODEL, context);
-        AttributeSetter.setEntityToContext(MANUFACTURER, context);
+        String daoType = RB.getString("dao.type");
+        if ("JDBC.H2".equals(daoType)) {
+            ServletContext context = sce.getServletContext();
+            AttributeSetter.setEntityToContext(COLOR, context);
+            AttributeSetter.setEntityToContext(MODEL, context);
+            AttributeSetter.setEntityToContext(MANUFACTURER, context);
+        }
     }
 
     @Override
