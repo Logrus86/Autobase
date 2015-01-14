b3e3dbf7591ac9385bce805e13df42062e949a2f
Logrus86 <logr86@gmail.com>
2015-01-14 18:03:13 +0600

    actions remake to @WebServlets

---
+++ src/main/java/com/epam/bp/autobase/servlet/LocaleServlet.java
@@ -0,0 +1,37 @@
+package com.epam.bp.autobase.servlet;
+
+import com.epam.bp.autobase.util.AttributeSetter;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Locale;
+
+@WebServlet("/do/locale")
+public class LocaleServlet extends HttpServlet{
+    private static final String REFERRER = "Referer";
+    private static final String LOCALE = "locale";
+    private static final String MSG = "Locale was changed to: ";
+    @Inject
+    AttributeSetter as;
+    @Inject
+    org.slf4j.Logger logger;
+
+    public LocaleServlet() {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String language = req.getParameter(LOCALE);
+        HttpSession session = req.getSession();
+        Locale locale = new Locale(language);
+        session.setAttribute(LOCALE, locale);
+        logger.info(MSG + language);
+        as.setToSession(AttributeSetter.COLOR, session);
+        resp.sendRedirect(req.getHeader(REFERRER));
+    }
+}
---
+++ src/main/java/com/epam/bp/autobase/servlet/LoginServlet.java
@@ -0,0 +1,9 @@
+package com.epam.bp.autobase.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("do/aaa")
+public class LoginServlet extends HttpServlet {
+
+}
