a98801cff6844c285ca8a17d086c732fcdfabd81
Logrus86 <logr86@gmail.com>
2015-01-16 12:39:18 +0600

    +produces userList, +scopes, +onUserListChanges

--- src/main/java/com/epam/bp/autobase/util/ListProducer.java
+++ src/main/java/com/epam/bp/autobase/util/ListProducer.java
@@ -1,24 +1,29 @@
 package com.epam.bp.autobase.util;
 
 import com.epam.bp.autobase.entity.*;
+import com.epam.bp.autobase.service.UserService;
+
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.event.Observes;
+import javax.enterprise.event.Reception;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.enterprise.event.Reception;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
-@RequestScoped
+@ApplicationScoped
 public class ListProducer {
     @Inject
     private EntityManager em;
+    @Inject
+    UserService us;
     private List<Color> colors;
     private List<Model> models;
     private List<Manufacturer> manufacturers;
-
+    private List<User> userList;
     @Produces
     @Named
     public Vehicle.Fuel[] getFuelTypes() {
@@ -41,25 +46,48 @@ public class ListProducer {
     }
     @Produces
     @Named
+    @RequestScoped
     public List<Color> getColors() {
         retrieveAllColors();
         return colors;
     }
     @Produces
     @Named
+    @RequestScoped
     public List<Model> getModels() {
         retrieveAllModels();
         return models;
     }
     @Produces
     @Named
+    @RequestScoped
     public List<Manufacturer> getManufacturers() {
         retrieveAllManufacturers();
         return manufacturers;
     }
 
+    @Produces
+    @Named
+    @RequestScoped
+    public List<User> getUserList() {
+        if (userList == null) {
+            retrieveAllUsers();
+        }
+        return userList;
+    }
+
+    private void retrieveAllUsers() {
+        TypedQuery<User> query = em.createNamedQuery("User.getAll", User.class);
+        userList = query.getResultList();
+    }
+
     private void retrieveAllColors() {
-        TypedQuery<Color> query = em.createNamedQuery("Color.getAll", Color.class);
+        TypedQuery<Color> query;
+        if ("ru".equals(us.getLocale().getLanguage())) {
+            query = em.createNamedQuery("Color.getAllSortedByRu", Color.class);
+        } else {
+            query = em.createNamedQuery("Color.getAllSortedByEn", Color.class);
+        }
         colors = query.getResultList();
     }
 
@@ -75,5 +103,9 @@ public class ListProducer {
 
     public void onMemberListChanged(@Observes(notifyObserver = Reception.IF_EXISTS) final Color color) {
         retrieveAllColors(); //TODO use this where specs are being changed
+    }
+
+    public void onUserListChanged(@Observes(notifyObserver = Reception.IF_EXISTS) final User user) {
+        retrieveAllUsers();
     }
 }
