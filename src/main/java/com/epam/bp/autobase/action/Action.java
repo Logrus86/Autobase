466f22149f8ad810f98f3bc3556d27c97aa4f2b8
Logrus86 <logr86@gmail.com>
2015-01-27 16:43:02 +0600

    -actions

--- src/main/java/com/epam/bp/autobase/action/Action.java
+++
@@ -1,7 +0,0 @@
-package com.epam.bp.autobase.action;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface Action {
-    ActionResult execute(HttpServletRequest request) throws ActionException;
-}
