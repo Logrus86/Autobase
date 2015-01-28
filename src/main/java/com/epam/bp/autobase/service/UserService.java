2ac21f0028492df5771500828e972ae5f5c28639
Logrus86 <logr86@gmail.com>
2015-01-28 17:30:04 +0600

    +unLinkVehicles() while deleting user

--- src/main/java/com/epam/bp/autobase/service/UserService.java
+++ src/main/java/com/epam/bp/autobase/service/UserService.java
@@ -1,15 +1,20 @@
 package com.epam.bp.autobase.service;
 
 import com.epam.bp.autobase.cdi.SessionState;
+import com.epam.bp.autobase.dao.DaoException;
 import com.epam.bp.autobase.dao.UserDao;
 import com.epam.bp.autobase.dao.hibernate.HibernateUserDao;
 import com.epam.bp.autobase.model.dto.UserDto;
 import com.epam.bp.autobase.model.entity.User;
+import com.epam.bp.autobase.model.entity.Vehicle;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.event.Event;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 @Model
 public class UserService extends AbstractService<User, UserDto, UserDao> {
@@ -40,12 +45,26 @@ public class UserService extends AbstractService<User, UserDto, UserDao> {
 
     @Override
     public void delete(Integer id) throws ServiceException {
+        try {
+            User user = dao.getById(id);
+            unLinkVehicles(user);
+        } catch (DaoException e) {
+            throw new ServiceException("User with id=" + id + " wasn't found. Abort.");
+        }
+
         delete(id, dao, event);
     }
 
     @Override
     public void delete(UserDto dto) throws ServiceException {
+        User user = getEntityFromDto(dto);
+        unLinkVehicles(user);
         delete(dto, dao, event);
+    }
+
+    private void unLinkVehicles(User user) {
+        if ((user.getRole().equals(User.Role.DRIVER)) & (!user.getVehicles().isEmpty()))
+            for (Vehicle vehicle : user.getVehicles()) vehicle.setDriver(null);
     }
 
     @Override
@@ -75,7 +94,7 @@ public class UserService extends AbstractService<User, UserDto, UserDao> {
                 .setDob(dto.getDobString())
                 .setEmail(dto.getEmail())
                 .setRole(dto.getRole())
-                .setBalance(dto.getBalance());
+                .setBalance(null);
         if (dto.getId() != null) {
             user.setId(dto.getId());
         }
@@ -86,146 +105,53 @@ public class UserService extends AbstractService<User, UserDto, UserDao> {
 
     @Override
     public String checkAllFieldsNotBusy(User user) throws ServiceException {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        Locale locale = ss.getLocale();
+        try {
+            if (dao.checkFieldValueExists(USERNAME, user.getUsername())) {
+                String error = ResourceBundle.getBundle(RB, locale).getString("error.busy-username");
+                sb.append(error);
+                getErrorMap().put(USERNAME + "_" + MSG, error);
+            }
+            if (dao.checkFieldValueExists(EMAIL, user.getEmail())) {
+                if (!"".equals(sb.toString())) sb.append("; ");
+                String error = ResourceBundle.getBundle(RB, locale).getString("error.busy-email");
+                sb.append(error);
+                getErrorMap().put(EMAIL + "_" + MSG, error);
+            }
+            return sb.toString();
+        } catch (DaoException e) {
+            e.printStackTrace();
+            throw new ServiceException(e.getMessage(), e.getCause());
+        }
     }
 
     @Override
     public String checkChangedFieldsNotBusy(User user, UserDto dto) throws ServiceException {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        Locale locale = ss.getLocale();
+        try {
+            if ((!user.getUsername().equals(dto.getUsername())) && (dao.checkFieldValueExists(USERNAME, user.getUsername()))) {
+                String error = ResourceBundle.getBundle(RB, locale).getString("error.busy-username");
+                sb.append(error);
+                if (getErrorMap() == null) setErrorMap(new HashMap<>());
+                getErrorMap().put(USERNAME + "_" + MSG, error);
+            }
+            if ((!user.getPassword().equals(dto.getPassword())) && (dao.checkFieldValueExists(PASSWORD, user.getPassword()))) {
+                if (!"".equals(sb.toString())) sb.append("; ");
+                String error = ResourceBundle.getBundle(RB, locale).getString("error.busy-password");
+                sb.append(error);
+                if (getErrorMap() == null) setErrorMap(new HashMap<>());
+                getErrorMap().put(PASSWORD + "_" + MSG, error);
+            }
+            return sb.toString();
+        } catch (DaoException e) {
+            e.printStackTrace();
+            throw new ServiceException(e.getMessage(), e.getCause());
+        }
     }
 
     public User findByCredentials(UserDto userDto) {
         return dao.findByCredentials(userDto.getUsername(), userDto.getPassword());
     }
-    
-    /* public void create(Map<String, String> userDataMap) throws ServiceException {
-        StringBuilder sb = new StringBuilder();
-        initNewUser();
-        user.setFirstname(userDataMap.get(FIRSTNAME))
-                .setLastname(userDataMap.get(LASTNAME))
-                .setDob(userDataMap.get(DOB))
-                .setUsername(userDataMap.get(USERNAME))
-                .setPassword(userDataMap.get(PASSWORD))
-                .setEmail(userDataMap.get(EMAIL))
-                .setRole(User.Role.CLIENT)
-                .setBalance(BigDecimal.ZERO);
-        //ejb validation
-        Set<ConstraintViolation<User>> cvs = validator().validateWhileCreate(user);
-        errorMap = userDataMap;
-        for (ConstraintViolation<User> cv : cvs) {
-            sb.append(cv.getMessage()).append(": ").append(cv.getInvalidValue()).append("; ");
-            errorMap.put(cv.getPropertyPath() + "_" + MSG, cv.getMessage());
-        }
-        //check password and password-repeat are the same
-        if (!userDataMap.get(PASSWORD).equals(userDataMap.get(PASSWORD_REPEAT))) {
-            String error = ResourceBundle.getBundle(RB).getString("error.passes-not-equals");
-            sb.append(error);
-            errorMap.put(PASSWORD + "_" + MSG, error);
-        }
-        // check busyness of username
-        if (checkFieldValueExists(USERNAME, user.getUsername())) {
-            String error = ResourceBundle.getBundle(RB).getString("error.busy-username");
-            sb.append(error);
-            errorMap.put(USERNAME + "_" + MSG, error);
-        }
-        // check busyness of email
-        if (checkFieldValueExists(EMAIL, user.getEmail())) {
-            String error = ResourceBundle.getBundle(RB).getString("error.busy-email");
-            sb.append(error);
-            errorMap.put(EMAIL + "_" + MSG, error);
-        }
-        // if sb is empty all is ok, proceed to persist
-        if (sb.toString().isEmpty()) {
-            em.persist(user);
-            ss.setSessionUser(user);
-            event.fire(user);
-            errorMap = null;
-            //...or clear user and throw exception
-        } else {
-            initNewUser();
-            throw new ServiceException("Error while user registration: " + sb.toString());
-        }
-    }
-
-    @PostConstruct
-    public void initNewUser() {
-        user = new User();
-    }
-
-    public boolean checkFieldValueExists(String field, String value) {
-        Session session = (Session) em.getDelegate();
-        Criteria criteria = session.createCriteria(User.class);
-        criteria.add(Restrictions.eq(field, value));
-        return !criteria.list().isEmpty();
-    }
-
-    public void update(Map<String, String> userDataMap) throws ServiceException {
-        StringBuilder sb = new StringBuilder();
-        User user = new User()
-                .setFirstname(userDataMap.get(FIRSTNAME))
-                .setLastname(userDataMap.get(LASTNAME))
-                .setDob(userDataMap.get(DOB))
-                .setEmail(userDataMap.get(EMAIL))
-                .setUsername(userDataMap.get(USERNAME))
-                .setPassword(userDataMap.get(PASSWORD));
-        String balance = userDataMap.get(BALANCE);
-        if (balance != null) user.setBalance(new BigDecimal(userDataMap.get(BALANCE)));
-        String role = userDataMap.get(ROLE);
-        if (role != null) user.setRole(role);
-        String idString = userDataMap.get(ID);
-        if (userDataMap.get(ID) != null) user.setId(Integer.parseInt(idString));
-        //ejb validation
-        errorMap = new HashMap<>();
-        Set<ConstraintViolation<User>> cvs = validator().validateWhileCreate(user);
-        for (ConstraintViolation<User> cv : cvs) {
-            sb.append(cv.getMessage()).append(": ").append(cv.getInvalidValue()).append("; ");
-            errorMap.put(cv.getPropertyPath() + "_" + MSG, cv.getMessage());
-            try {
-                String propertyValue = String.valueOf(User.class.getMethod("get" + cv.getPropertyPath()).invoke(null));
-                errorMap.put(cv.getPropertyPath().toString(), propertyValue);
-            } catch (Exception e) {
-                throw new ServiceException(e.getMessage());
-            }
-        }
-        // if username was changed
-        if (!em.find(User.class, user.getId()).getUsername().equals(user.getUsername())) {
-            // check busyness of new username
-            if (checkFieldValueExists(USERNAME, user.getUsername())) {
-                String error = ResourceBundle.getBundle(RB).getString("error.busy-username");
-                sb.append(error);
-                errorMap.put(USERNAME + "_" + MSG, error);
-            }
-        }
-        // if email was changed
-        if (!em.find(User.class, user.getId()).getEmail().equals(user.getEmail())) {
-            //check busyness of new email
-            if (checkFieldValueExists(EMAIL, user.getEmail())) {
-                String error = ResourceBundle.getBundle(RB).getString("error.busy-email");
-                sb.append(error);
-                errorMap.put(EMAIL + "_" + MSG, error);
-            }
-        }
-        // if sb is empty all is ok, proceed to persist
-        if (sb.toString().isEmpty()) {
-            em.merge(user);
-            event.fire(user);
-            errorMap = null;
-            //...or throw ServiceException
-        } else {
-            throw new ServiceException("Error while user updating: " + sb.toString());
-        }
-    }
-
-    public void delete(Integer id) {
-        // retrieve user by id
-        User userToDelete = em.find(User.class, id);
-        // unlink his vehicles if he is driver
-        if ((userToDelete.getRole().equals(User.Role.DRIVER)) & (!userToDelete.getVehicles().isEmpty())) {
-            for (Vehicle vehicle : userToDelete.getVehicles()) {
-                vehicle.setDriver(null);
-            }
-        }
-        em.remove(userToDelete);
-        event.fire(userToDelete);
-    }*/
 }
