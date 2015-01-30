711d7c5bf9a05adc94db63b76fc3b92bfefd4aeb
Logrus86 <logr86@gmail.com>
2015-01-30 13:36:39 +0600

    extends BaseDao<T>

--- src/main/java/com/epam/bp/autobase/dao/ManufacturerDao.java
+++ src/main/java/com/epam/bp/autobase/dao/ManufacturerDao.java
@@ -2,7 +2,7 @@ package com.epam.bp.autobase.dao;
 
 import com.epam.bp.autobase.model.entity.Manufacturer;
 
-public interface ManufacturerDao {
+public interface ManufacturerDao extends BaseDao<Manufacturer>{
 
     public Manufacturer getByValue(String value) throws DaoException;
 
--- src/main/java/com/epam/bp/autobase/dao/ModelDao.java
+++ src/main/java/com/epam/bp/autobase/dao/ModelDao.java
@@ -2,7 +2,7 @@ package com.epam.bp.autobase.dao;
 
 import com.epam.bp.autobase.model.entity.Model;
 
-public interface ModelDao {
+public interface ModelDao extends BaseDao<Model> {
 
     public Model getByValue(String value) throws DaoException;
 
