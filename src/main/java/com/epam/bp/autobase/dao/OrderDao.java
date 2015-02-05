d699c9cf576e8e79d0a0773be0348dc4e224d24e
Logrus86 <logr86@gmail.com>
2015-02-05 11:55:31 +0600

    extends BaseDao

--- src/main/java/com/epam/bp/autobase/dao/OrderDao.java
+++ src/main/java/com/epam/bp/autobase/dao/OrderDao.java
@@ -4,10 +4,9 @@ import com.epam.bp.autobase.model.entity.Order;
 import com.epam.bp.autobase.model.entity.User;
 import com.epam.bp.autobase.model.entity.Vehicle;
 
-import java.util.Date;
 import java.util.List;
 
-public interface OrderDao {
+public interface OrderDao extends BaseDao<Order> {
 
     public List<Order> getClientOrders(User client) throws DaoException;
 
@@ -17,6 +16,6 @@ public interface OrderDao {
 
     public List<Order> getVehicleOrders(Integer vehicleId) throws DaoException;
 
-    public Order getByDateOrdered(Date date) throws DaoException;
+    public Order getByDateOrdered(String dateString) throws DaoException;
 
 }
