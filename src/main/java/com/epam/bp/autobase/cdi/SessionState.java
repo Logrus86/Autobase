8b8a80812aa694d45f8bb39ea291cda4d6c99d63
Logrus86 <logr86@gmail.com>
2015-02-19 17:27:42 +0600

    -event

--- src/main/java/com/epam/bp/autobase/cdi/SessionState.java
+++ src/main/java/com/epam/bp/autobase/cdi/SessionState.java
@@ -1,16 +1,13 @@
 package com.epam.bp.autobase.cdi;
 
-import com.epam.bp.autobase.model.entity.Color;
 import com.epam.bp.autobase.model.entity.User;
 import org.jboss.logging.Logger;
 
 import javax.ejb.Stateful;
 import javax.enterprise.context.SessionScoped;
-import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
-import java.util.LinkedHashMap;
 import java.util.Locale;
 import java.util.Map;
 
@@ -19,8 +16,6 @@ import java.util.Map;
 @SessionScoped
 public class SessionState implements Serializable {
     private static Map<String, String> langs;
-    @Inject
-    Event<Color> event;
     @Inject
     Logger logger;
     private User sessionUser;
@@ -32,19 +27,6 @@ public class SessionState implements Serializable {
 
     public void setSessionUser(User sessionUser) {
         this.sessionUser = sessionUser;
-    }
-
-    public Map<String, String> getLangs() {
-        if (langs == null) {
-            langs = new LinkedHashMap<>();
-            langs.put("English", "en");
-            langs.put("Русский", "ru");
-        }
-        return langs;
-    }
-
-    public void setLangs(Map<String, String> langs) {
-        SessionState.langs = langs;
     }
 
     public Locale getLocale() {
@@ -66,7 +48,5 @@ public class SessionState implements Serializable {
     public void changeLocale(String lang_code) {
         this.locale = new Locale(lang_code);
         logger.info("Locale was changed to: " + lang_code);
-        //fires entities which list depends of locale:
-        event.fire(new Color());
     }
 }
