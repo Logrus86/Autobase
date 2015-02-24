4a0cf9d109ea0f39f16875fdfc9ed2df84c28d4e
Logrus86 <logr86@gmail.com>
2015-02-24 17:18:02 +0600

    +buildUserDtoGwt(UserDto userDto), switch for roles to get Integer

--- src/main/java/com/epam/bp/autobase/gwt/server/ServerUtils.java
+++ src/main/java/com/epam/bp/autobase/gwt/server/ServerUtils.java
@@ -1,12 +1,13 @@
 package com.epam.bp.autobase.gwt.server;
 
 import com.epam.bp.autobase.gwt.dto.UserDtoGwt;
+import com.epam.bp.autobase.model.dto.UserDto;
 import com.epam.bp.autobase.model.entity.User;
 
 public class ServerUtils {
     public static UserDtoGwt buildUserDtoGwt(User user) {
         if (user == null) return null;
-        return new UserDtoGwt()
+        UserDtoGwt result = new UserDtoGwt()
                 .setId(user.getId())
                 .setUsername(user.getUsername())
                 .setPassword(user.getPassword())
@@ -14,7 +15,37 @@ public class ServerUtils {
                 .setLastname(user.getLastname())
                 .setDob(user.getDobString())
                 .setEmail(user.getEmail())
-                .setRole(user.getRoleInteger())
                 .setBalance(user.getBalance());
+        switch (user.getRole()) {
+            case ADMIN: result.setRole(0);
+                break;
+            case CLIENT: result.setRole(1);
+                break;
+            case DRIVER: result.setRole(2);
+                break;
+        }
+        return result;
+    }
+
+    public static UserDtoGwt buildUserDtoGwt(UserDto userDto) {
+        if (userDto == null) return null;
+        UserDtoGwt result = new UserDtoGwt()
+                .setId(userDto.getId())
+                .setUsername(userDto.getUsername())
+                .setPassword(userDto.getPassword())
+                .setFirstname(userDto.getFirstname())
+                .setLastname(userDto.getLastname())
+                .setDob(userDto.getDob())
+                .setEmail(userDto.getEmail())
+                .setBalance(userDto.getBalance());
+        switch (userDto.getRole()) {
+            case ADMIN: result.setRole(0);
+                break;
+            case CLIENT: result.setRole(1);
+                break;
+            case DRIVER: result.setRole(2);
+                break;
+        }
+        return result;
     }
 }
