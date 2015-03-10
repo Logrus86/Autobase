fd9aaea0cf11d43b84bd96bc60ff3dbcc24bb764
Logrus86 <logr86@gmail.com>
2015-03-10 15:26:49 +0600

    GenerationType.IDENTITY only works with gwt

--- src/main/java/com/epam/bp/autobase/model/entity/User.java
+++ src/main/java/com/epam/bp/autobase/model/entity/User.java
@@ -22,7 +22,7 @@ public class User implements Identifiable<User> {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String uuid;
     @NotEmpty
