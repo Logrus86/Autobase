07cb17c12eec2475a868abcd6c86976990e01dd8
Logrus86 <logr86@gmail.com>
2015-03-10 15:26:23 +0600

    deleted redundant lines

--- src/main/java/com/epam/bp/autobase/gwt/server/RegisterServiceImpl.java
+++ src/main/java/com/epam/bp/autobase/gwt/server/RegisterServiceImpl.java
@@ -3,15 +3,12 @@ package com.epam.bp.autobase.gwt.server;
 import com.epam.bp.autobase.cdi.SessionState;
 import com.epam.bp.autobase.gwt.client.rpc.RegisterService;
 import com.epam.bp.autobase.model.dto.UserDto;
-import com.epam.bp.autobase.service.ServiceException;
 import com.epam.bp.autobase.service.UserService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import org.jboss.logging.Logger;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-@ApplicationScoped
 public class RegisterServiceImpl extends RemoteServiceServlet implements RegisterService {
     @Inject
     Logger logger;
@@ -23,13 +20,11 @@ public class RegisterServiceImpl extends RemoteServiceServlet implements Registe
     @Override
     public UserDto register(UserDto userDto) {
         try {
-
             us.create(userDto);
-
             ss.setSessionUser(userDto.buildLazyEntity());
             logger.info("Newly registered user: " + ss.getSessionUser().toString());
             return userDto;
-        } catch (ServiceException e) {
+        } catch (Exception e) {
             logger.error("Registration error: " + e.getMessage());
         }
         return null;
