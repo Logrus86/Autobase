edfff8dded05a8e592fd9d8aee39188a6c7adb93
Logrus86 <logr86@gmail.com>
2015-03-16 17:15:10 +0600

    +findVehicle(VehicleDto dto)

--- src/main/java/com/epam/bp/autobase/gwt/client/rpc/FetchService.java
+++ src/main/java/com/epam/bp/autobase/gwt/client/rpc/FetchService.java
@@ -3,6 +3,8 @@ package com.epam.bp.autobase.gwt.client.rpc;
 import com.epam.bp.autobase.model.dto.ColorDto;
 import com.epam.bp.autobase.model.dto.ManufacturerDto;
 import com.epam.bp.autobase.model.dto.ModelDto;
+import com.epam.bp.autobase.model.dto.VehicleDto;
+import com.epam.bp.autobase.model.entity.Vehicle;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
@@ -17,6 +19,8 @@ public interface FetchService extends RemoteService {
     List<ManufacturerDto> fetchManufacturers();
 
     List<ColorDto> fetchColors();
+
+    Vehicle findVehicle(VehicleDto dto);
 
     static class App {
         private static FetchServiceAsync ourInstance = GWT.create(FetchService.class);
--- src/main/java/com/epam/bp/autobase/gwt/client/rpc/FetchServiceAsync.java
+++ src/main/java/com/epam/bp/autobase/gwt/client/rpc/FetchServiceAsync.java
@@ -3,6 +3,8 @@ package com.epam.bp.autobase.gwt.client.rpc;
 import com.epam.bp.autobase.model.dto.ColorDto;
 import com.epam.bp.autobase.model.dto.ManufacturerDto;
 import com.epam.bp.autobase.model.dto.ModelDto;
+import com.epam.bp.autobase.model.dto.VehicleDto;
+import com.epam.bp.autobase.model.entity.Vehicle;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 import java.util.List;
@@ -13,4 +15,6 @@ public interface FetchServiceAsync {
     void fetchManufacturers(AsyncCallback<List<ManufacturerDto>> async);
 
     void fetchColors(AsyncCallback<List<ColorDto>> async);
+
+    void findVehicle(VehicleDto dto, AsyncCallback<Vehicle> async);
 }
