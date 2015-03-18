2962868ff54f937ade051aafcf7245c1c9714f0d
Logrus86 <logr86@gmail.com>
2015-03-18 16:47:02 +0600

    ServerUtil returned

---
+++ src/main/java/com/epam/bp/autobase/gwt/server/ServerUtil.java
@@ -0,0 +1,21 @@
+package com.epam.bp.autobase.gwt.server;
+
+import com.epam.bp.autobase.model.dto.UserDto;
+import com.epam.bp.autobase.model.entity.User;
+import com.epam.bp.autobase.util.DateParser;
+
+public class ServerUtil {
+    public static UserDto buildUserDtoGwt(User user) {
+        if (user == null) return null;
+        return new UserDto()
+                .setId(user.getId())
+                .setUsername(user.getUsername())
+                .setPassword(user.getPassword())
+                .setFirstname(user.getFirstname())
+                .setLastname(user.getLastname())
+                .setDob(DateParser.stringFromDate(user.getDob()))
+                .setEmail(user.getEmail())
+                .setBalance(user.getBalance())
+                .setRole(user.getRole());
+    }
+}
