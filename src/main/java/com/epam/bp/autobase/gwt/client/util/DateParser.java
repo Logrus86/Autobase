eb5f3c3bb83d2efe737a1c537cd7854d71612976
Logrus86 <logr86@gmail.com>
2015-03-20 12:02:00 +0600

    gtw\util\DateParser -> gtw\client\util\DateParser

---
+++ src/main/java/com/epam/bp/autobase/gwt/client/util/DateParser.java
@@ -0,0 +1,42 @@
+package com.epam.bp.autobase.gwt.client.util;
+
+import com.google.gwt.i18n.client.DateTimeFormat;
+
+import java.util.Date;
+
+// GWT-analogue of com.epam.bp.autobase.util.DateParser, uses [GWT] DateTimeFormat instead of [JRE] SimpleDateFormat
+public class DateParser {
+
+    private static final String TIMESTAMP_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
+    private static final String DATE_DB_PATTERN = "yyyy-MM-dd";
+
+    public static Date dateFromString(String formattedString) {
+        if (formattedString == null) return null;
+        DateTimeFormat dtf = new DateTimeFormat(DATE_PATTERN) {
+        };
+        return dtf.parse(formattedString);
+    }
+
+    public static String stringFromDate(Date date) {
+        if (date == null) return null;
+        DateTimeFormat dtf = new DateTimeFormat(DATE_PATTERN) {
+        };
+        return dtf.format(date);
+    }
+
+    public static String stringFromDateTime(Date dateWithTime) {
+        if (dateWithTime == null) return null;
+        DateTimeFormat dtf = new DateTimeFormat(TIMESTAMP_PATTERN) {
+        };
+        return dtf.format(dateWithTime);
+    }
+
+    public static Date dateTimeFromString(String dateWithTime) {
+        if (dateWithTime == null) return null;
+        DateTimeFormat dtf = new DateTimeFormat(TIMESTAMP_PATTERN) {
+        };
+        return dtf.parse(dateWithTime);
+    }
+
+}
